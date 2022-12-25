40ddad6dff69ba0979bdd9b2c658a2d0e4dd841a
Ayaz Mirza <37616943+ayazmirza54@users.noreply.github.com>
2022-12-25 20:38:29 +0000

    added web vitals tracking

--- client/script.js
+++ client/script.js
@@ -1,5 +1,17 @@
 import bot from './assets/bot.svg'
 import user from './assets/user.svg'
+import {onCLS, onFID, onLCP} from 'web-vitals';
+
+function sendToAnalytics(metric) {
+  const body = JSON.stringify(metric);
+    // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+      (navigator.sendBeacon && navigator.sendBeacon('/analytics', body)) ||
+          fetch('/analytics', {body, method: 'POST', keepalive: true});
+          }
+
+          onCLS(sendToAnalytics);
+          onFID(sendToAnalytics);
+          onLCP(sendToAnalytics);
 
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
